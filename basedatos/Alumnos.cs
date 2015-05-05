9cef9f11f5cce5052cb54231594770b72a010da3
angelaguayo <anggelagguayoo@gmail.com>
2015-05-05 18:10:41 -0500

    ahora si
    terminada

---
+++ basedatos/Alumnos.cs
@@ -0,0 +1,76 @@
+﻿using System;
+using MySql.Data.MySqlClient;
+namespace basedatos
+{
+	public class Alumnos : MySQL
+	{
+		public Alumnos ()
+		{
+		}
+		
+		public void mostrarTodos(){
+			this.abrirConexion();
+            MySqlCommand myCommand = new MySqlCommand(this.querySelect(), 
+			                                          myConnection);
+			Console.WriteLine("+-----------------------------------+");
+            MySqlDataReader myReader = myCommand.ExecuteReader();	
+	        while (myReader.Read()){
+	            string id = myReader["id"].ToString();
+	            string telefono = myReader["telefono"].ToString();
+	            string nombre = myReader["nombre"].ToString();
+	            Console.WriteLine("|ID: " + id +"\n"+
+				                  "|telefono: " + telefono +"\n"+
+				                  "|Nombre: " + nombre+"\n|");
+				                  
+	       }
+			
+            myReader.Close();
+			myReader = null;
+            myCommand.Dispose();
+			myCommand = null;
+			Console.WriteLine("+-----------------------------------+");
+			this.cerrarConexion();
+		}
+		
+		public void insertarRegistroNuevo(string nombre, string telefono, string id){
+			this.abrirConexion();
+			string sql = "INSERT INTO `personas` VALUES ('" + nombre + "', '" + telefono + "','"+id+"')";
+			this.ejecutarComando(sql);
+			this.cerrarConexion();
+		}
+		
+		public void editarTelefonoRegistro(string id, string telefono){
+			this.abrirConexion();
+			string sql = "UPDATE `personas` SET `telefono`='" + telefono + "' WHERE (`id`='" + id + "')";
+			this.ejecutarComando(sql);
+			this.cerrarConexion();
+		}
+		
+		public void editarNombreRegistro(string id, string nombre){
+			this.abrirConexion();
+			string sql = "UPDATE `personas` SET `nombre`='" + nombre + "' WHERE (`id`='" + id + "')";
+			this.ejecutarComando(sql);
+			this.cerrarConexion();
+		}
+		
+		public void eliminarRegistroPorId(string id){
+			this.abrirConexion();
+			string sql = "DELETE FROM `personas` WHERE (`id`='" + id + "')";
+			this.ejecutarComando(sql);
+			this.cerrarConexion();
+		}
+		private int ejecutarComando(string sql){
+			MySqlCommand myCommand = new MySqlCommand(sql,this.myConnection);
+			int afectadas = myCommand.ExecuteNonQuery();
+			myCommand.Dispose();
+			myCommand = null;
+			return afectadas;
+		}
+		
+		private string querySelect(){
+			return "SELECT * " +
+	           	"FROM personas";
+		}
+	}
+}
+
---
+++ basedatos/Program.cs
@@ -0,0 +1,64 @@
+﻿using System;
+
+namespace basedatos
+{
+	class MainClass
+	{
+		public static void Main (string[] args)
+		{
+			int menu;
+			string nombre;
+			string telefono,id;
+			Alumnos alumnos = new Alumnos();
+			do{
+			Console.WriteLine("co co comenzamos\n1-agregar\n2-mostrar\n3-editar  por id\n4-eliminar\n");
+				menu=int.Parse(Console.ReadLine());
+			switch(menu){
+				case 1: 
+					Console.WriteLine("nombre?");
+					nombre=Console.ReadLine();
+					Console.WriteLine("telefono?");
+					telefono=Console.ReadLine();
+					Console.WriteLine("id?");
+					id=Console.ReadLine();
+					alumnos.insertarRegistroNuevo(nombre,telefono,id);
+					nombre="";
+					telefono="";
+					id="";
+				break;
+				
+				case 2: alumnos.mostrarTodos();
+		
+				break;
+				
+				case 3: 
+				Console.WriteLine("modificar? \n1-nombre\n2-telefono\n3-salir\n");
+				int menu2 = int.Parse(Console.ReadLine());
+				switch(menu2){
+						case 1 :
+							Console.WriteLine("id?");
+							id=Console.ReadLine();
+							Console.WriteLine("nombre a modificar?");
+							nombre=Console.ReadLine();
+							alumnos.editarNombreRegistro(id,nombre);
+							break;
+						case 2 :Console.WriteLine("id?");
+							id=Console.ReadLine();
+							Console.WriteLine("telefono a modificar?");
+							telefono=Console.ReadLine();
+							alumnos.editarTelefonoRegistro(id,telefono);
+							break;
+				}
+							break;
+				
+				case 4:
+					Console.WriteLine("id?");
+					id=Console.ReadLine();
+					alumnos.eliminarRegistroPorId(id);
+					break;
+			}
+			}while(menu != 5);
+			
+		}
+	}
+}
